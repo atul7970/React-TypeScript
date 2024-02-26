dd9aacebe8a4eca058e71839b9b3c252d1a7cddd
atul7970 <104618349+atul7970@users.noreply.github.com>
2024-02-26 19:07:59 +0530

    useState-Examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import { useState } from "react";
-import Box from "./components/Box";
+import { FormEvent, useState } from "react";
+// import Box from "./components/Box";
+interface Person {
+  name: string;
+  age: number;
+}
+
 function App() {
-  const [val, setVal] = useState<string>("");
+  const [user, setUser] = useState<Person>();
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(user);
+  };
   return (
-    <>
-      <div>
-        <Box label="Search" value={val} setter={setVal} />
-      </div>
-    </>
+    <div>
+      {/* <Box label="Search" value={val} setter={setVal} /> */}
+      <form onSubmit={submitHandler}>
+        <input
+          type="number"
+          placeholder="Age"
+          value={user?.age || ""}
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev!, age: Number(e.target.value) }))
+          }
+        />
+        <input
+          type="text"
+          placeholder="Name"
+          value={user?.name || ""}
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev!, name: e.target.value }))
+          }
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 export default App;
