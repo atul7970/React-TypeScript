5efacb32ae4823fd12ca7146cb0079a086958291
atul7970 <104618349+atul7970@users.noreply.github.com>
2024-02-27 22:55:50 +0530

    useReducer hook

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@
 //   age: number;
 // }
 
+import { useReducer } from "react";
+
 // function App() {
 //   const [user, setUser] = useState<Person>();
 //   const submitHandler = (e: FormEvent<HTMLFormElement>) => {
@@ -69,8 +71,54 @@
 //   );
 // };
 
+type StateType = {
+  count: number;
+};
+
+type ActionType =
+  | { type: "Increment"; payload: number }
+  | { type: "Decrement"; payload: number };
+
+const initialState: StateType = {
+  count: 0,
+};
+const reducer = (state: StateType, action: ActionType) => {
+  switch (action.type) {
+    case "Increment":
+      return { count: state.count + action.payload };
+    case "Decrement":
+      return { count: state.count - action.payload };
+
+    default:
+      return state;
+  }
+};
+
 function App() {
-  return <div>useReducer Hook</div>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const increment = (): void => {
+    dispatch({
+      type: "Increment",
+      payload: 1,
+    });
+  };
+  const decrement = (): void => {
+    dispatch({
+      type: "Decrement",
+      payload: 1,
+    });
+  };
+
+  return (
+    <div>
+      <h1>Count Change</h1>
+      <p>Count : {state.count}</p>
+
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
 }
 
 export default App;
